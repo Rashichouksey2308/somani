72173541ef926c64bca75fb03d80a9023a283a1b
VishwaVijay <vishwavijay.bisht@techchefz.com>
2022-05-17 14:13:54 +0530

    first page

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,45 @@ import { createStore } from "store";
 import { getReposAsync } from "features/repoSearch/repoSearchSlice";
 import DoughnutCart from "../src/components/DoughnutCart/index";
 import Leads from "../src/components/Leads/index";
+import styles from "./index.module.scss";
 
 import "bootstrap/dist/css/bootstrap.css";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const IndexPage = () => {
   const router = useRouter();
   return (
     <>
-      <div className={styles.root_Container}></div>
+      <div className={styles.root_Container}>
+        <div className={styles.head_Container}>
+          <Leads />
+          <Leads />
+        </div>
+        <div className={styles.bottom_Container}>
+          <div className={styles.left_Container}>
+            <Card
+              style={{ width: "406px", height: "401px" }}
+              className={styles.card}
+            />
+            <Card
+              style={{ width: "406px", height: "247px" }}
+              className={styles.card}
+            />
+          </div>
+          <div className={styles.right_Container}>
+            <div className={styles.upper_Container}>
+              <DoughnutCart />
+              <DoughnutCart />
+            </div>
+            <div className={styles.bottom_Container}>
+              <Card
+                style={{ width: "1090px", height: "369px" }}
+                className={styles.card}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
