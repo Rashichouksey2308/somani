876881e42b55a29ecca2027712b8a14b15c763d6
VishwaVijay <vishwavijay.bisht@techchefz.com>
2022-05-19 11:22:34 +0530

    css fix

--- src/components/CompanyProfile/index.jsx
+++ src/components/CompanyProfile/index.jsx
@@ -7,22 +7,22 @@ function index() {
  
   return (
       <Card className={styles.card}>
-        <Card.Header className={styles.head}>
+        <Card.Header className={styles.header}>
          <span>Company Profile</span>   
          <span class={styles.addicon}>+</span>     
         </Card.Header>
         {/* <hr className={styles.hr}/> */}
         <Card.Body className={styles.body}>
             {fields("Company Name","Ramakrishna Traders",false)}
-            {fields("Company Name","Ramakrishna Traders",false)}
-            {fields("Company Name","Ramakrishna Traders",false)}
-            {fields("Company Name","Ramakrishna Traders",false)}
-            {fields("Company Name","Ramakrishna Traders",false)}
-            {fields("Company Name","Ramakrishna Traders",false)}
+            {fields("Company PAN","1123456780",false)}
+            {fields("Type Of Business","Manufacturer",false)}
+            {fields("Transaction Type","International",false)}
+            {fields("Turn-Over (in Cr)","60",false)}
+            {fields("Email ID","Johndoe@Email.Com",false)}
                             
-            {fields("Company Name","Ramakrishna Traders",false)}
-            {fields("Company Name","Ramakrishna Traders",false)}
-            {fields("Company Name","Ramakrishna Traders",true)}
+            {fields("Phone Number","+91 9876543210",false)}
+            {fields("Whatsapp Number","+91 9876543210",false)}
+            {fields("Communication Mode","Email, Whatsapp",true)}
                         
         </Card.Body>
        
