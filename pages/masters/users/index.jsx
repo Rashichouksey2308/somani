826ae908a7d0939e1893cb3320a599f6ab09a992
Sneha <sneha.yadav@techchefz.com>
2022-06-08 16:32:17 +0530

    build(users ,leads): Dark mode BREAKING CHANGE:

--- pages/masters/users/index.jsx
+++ pages/masters/users/index.jsx
@@ -4,7 +4,7 @@ import styles from './index.module.scss'
 const index = () => {
   return (
     <>
-    <div className='container-fluid'>
+    <div className='container-fluid card'>
         {/*filter*/}
         <div className={`${styles.filter} d-flex align-items-center`}>
           
@@ -13,39 +13,41 @@ const index = () => {
                 <div className={`${styles.inputGroupPrepend} input-group-prepend`}>
                   <img src="/static/search.svg" className="img-fluid" alt="Search" />
                 </div>
-                <input type="text" className={`${styles.formControl} form-control`} placeholder="Search" />
+                <input type="text" className={`${styles.formControl} formControl form-control`} placeholder="Search" />
               </div>
             </div>
             <a className={styles.filterIcon}><img src="/static/filter.svg" className="img-fluid" alt="Filter" /></a>
-            <a href="#" className={styles.filterList}>From: 22-02-2022 To: 22-04-2022 <img src="/static/close-b.svg" className="img-fluid" alt="Close"/></a>
-            <a href="#" className={styles.filterList}>Raj<img src="/static/close-b.svg" className="img-fluid" alt="Close" /></a>
+            <a href="#" className={`${styles.filterList} filterList`}>
+              From: 22-02-2022 To: 22-04-2022 <img src="/static/close-b.svg" className="img-fluid" alt="Close"/></a>
+            <a href="#" className={`${styles.filterList} filterList`}>
+              Raj<img src="/static/close-b.svg" className="img-fluid" alt="Close" /></a>
            
             <button type="button" className={`${styles.btnPrimary} btn ml-auto btn-primary`}>Add</button>   
           </div>
 
           {/*UserTable*/}
-          <div className={`${styles.datatable} mt-4`}>
+          <div className={`${styles.datatable} datatable mt-4`}>
             <div className={`${styles.tableFilter} d-flex justify-content-between`}>
-              <h3>Users</h3>
+              <h3 className='heading_card'>Users</h3>
               <div className={`${styles.pageList} d-flex justify-content-end align-items-center`}>
-                <span>Showing Page 1 out of 10</span>
-                <a href="#" className={`${styles.arrow} ${styles.leftArrow}`}><img src="/static/keyboard_arrow_right-3.svg" alt="arrow left" className="img-fluid"/></a>
-                <a href="#" className={`${styles.arrow} ${styles.rightArrow}`}><img src="/static/keyboard_arrow_right-3.svg" alt="arrow right" className="img-fluid"/></a>
+                <span>Showing Page 1 out of 10</span> 
+                <a href="#" className={`${styles.arrow} ${styles.leftArrow} arrow`}><img src="/static/keyboard_arrow_right-3.svg" alt="arrow left" className="img-fluid"/></a>
+                <a href="#" className={`${styles.arrow} ${styles.rightArrow} arrow`}><img src="/static/keyboard_arrow_right-3.svg" alt="arrow right" className="img-fluid"/></a>
               </div>
             </div>
             <table className={`${styles.table} table`} cellpadding="0" cellspacing="0" border="0">
               <thead>
                 <tr>
-                  <th>USER ID</th>
-                  <th>USER NAME</th>
-                  <th>CONTACT NUMBER</th>
-                  <th>DEPARTMENT</th>
-                  <th>CREATION DATE</th>
-                  <th>STATUS</th>
+                  <th className={`${styles.table_heading} table_heading`}>USER ID</th>
+                  <th className={`${styles.table_heading} table_heading`}>USER NAME</th>
+                  <th className={`${styles.table_heading} table_heading`}>CONTACT NUMBER</th>
+                  <th className={`${styles.table_heading} table_heading`}>DEPARTMENT</th>
+                  <th className={`${styles.table_heading} table_heading`}>CREATION DATE</th>
+                  <th className={`${styles.table_heading} table_heading`}>STATUS</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
+                <tr >
                   <td>SG1234</td>
                   <td className={styles.buyerName}>Rajsekhar</td>
                   <td>9034894802</td>
