8f3da124d7393d36839369412ec2c13e8dd64011
VishwaVijay <vishwavijay.bisht@techchefz.com>
2022-06-20 17:31:09 +0530

    feat(Common Save button and Financials break): BREAKING CHANGE:

---
+++ src/components/CommonSave/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './index.module.scss'
+function index() {
+  return (
+     <div className={`${styles.main} card border_color`}>
+       
+       <div className={styles.submit}><span>Save</span></div>
+      
+      
+      </div>
+  )
+}
+
+export default index
