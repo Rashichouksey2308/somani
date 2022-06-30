26c61dd0f78d8d77b86b54f9388518a0b56e8cca
Sneha <sneha.yadav@techchefz.com>
2022-06-30 13:09:22 +0530

    fix(QPA, AssignmentLetter): bugs fixed BREAKING CHANGE:

--- src/components/AssignmentLetter/index.jsx
+++ src/components/AssignmentLetter/index.jsx
@@ -131,13 +131,13 @@ const assignment=()=>{
      <div className="card-body">
       <h5 className={styles.sub_heading}>Assignment Letter between: </h5>
 
-      <input  type="text" className={`${styles.para} w-100`} placeholder = "Indo International Trading FZCO, Jafza View–18, LOB-180504, Jebel Ali, Dubai, UAE," />
-      <input  type="text" className={`${styles.para} w-100`} placeholder = "Hira Power and Steels Ltd, 511/1, 512/2, Urla Industrial Complex, Raipur – 493221, C.G., India (“the Buyer”)" />
+      <GrowInput  type="text" className={`${styles.para} `} />
+      <GrowInput  type="text" className={`${styles.para} `} />
       
       <p>and</p>
-      <p><GrowInput placeholder={`ERAMET MARKETING SERVICES, a limited liability company (SAS)`} type="text"/>, registered under the Trade Register of Paris under the number<input  type="text" className={`${styles.para} `} placeholder= "829 256 049" />, whose head office is at <input  className={`${styles.para} w-100`} placeholder = "10 boulevard de Grenelle – CS 63205 – 75015 Paris – FRANCE (“the Seller or Producer”)" />.
+      <p><GrowInput type="text"/>, registered under the Trade Register of Paris under the number<GrowInput  type="text" className={`${styles.para} `}  />, whose head office is at <GrowInput  className={`${styles.para} `} eholder = "10 boulevard de Grenelle – CS 63205 – 75015 Paris – FRANCE (“the Seller or Producer”)" />.
       </p>
-      <p>It is hereby agreed that the Seller will accept that the payment for the commodity - approximately <input  type="text"  className={`${styles.para} w-100`} placeholder="5,000 (+/-10%) MT of Manganese Ore of Gabon Origin (Grade: MMD) (“Goods”)" /> under the  <input  type="text"  className={`${styles.para} w-75`} placeholder="Contract No. 21-133 Dated 16.08.2021 (“Sales Contract”)"/> by and between the Seller and Hira Power and Steels Ltd (“Buyer”) is to be made by way of a Letter of Credit (L/C), to be issued on the applicant of Indo International Trading FZCO as per the above-mentioned Contract.  The commodity is for use by Hira Power and Steels Ltd (“the Buyer”) only under the terms and conditions contained within the Sales Contract.
+      <p>It is hereby agreed that the Seller will accept that the payment for the commodity - approximately <GrowInput  type="text"  className={`${styles.para} `}  /> under the  <GrowInput  type="text"  className={`${styles.para} `}/> by and between the Seller and Hira Power and Steels Ltd (“Buyer”) is to be made by way of a Letter of Credit (L/C), to be issued on the applicant of Indo International Trading FZCO as per the above-mentioned Contract.  The commodity is for use by Hira Power and Steels Ltd (“the Buyer”) only under the terms and conditions contained within the Sales Contract.
       </p>
       <p>The Buyer hereby confirms to remain responsible for the performance of the said contact, including any failure or delay in the issuance of the L/C in accordance with the terms of the contract and this letter.  Further Hira Power and Steels Ltd (“Buyer”) shall remain ultimately responsible for payment of the price in the event that the Seller is unable to obtain payment under the L/C, and hereby agree to indemnify the Seller for any loss, damage or expense including, without limitation, any liability Eramet Marketing Services (“the Seller”) may incur to Indo International Trading FZCO by reason of the invoice being addressed to Indo International Trading FZCO.
       </p>
@@ -154,14 +154,14 @@ const assignment=()=>{
         <strong>………………………………………………………</strong>
       <br/>
       <strong>………………………………………………………</strong>
-      <input type="text" className={`${styles.para}`} placeholder='For INDO INTERNATIONAL TRADING FZCO. (BUYER)'/>
+      <GrowInput type="text" className={`${styles.para}`} placeholder='For INDO INTERNATIONAL TRADING FZCO. (BUYER)'/>
       </div>
-      <input type="text" className={`${styles.para} pt-5`} placeholder='For INDO INTERNATIONAL TRADING FZCO. (BUYER)'/>
+      <GrowInput type="text" className={`${styles.para} pt-5`} placeholder='For INDO INTERNATIONAL TRADING FZCO. (BUYER)'/>
       </div>
       <p>
       <strong>………………………………………………………</strong>
       <br/>
-      <input type="text" className={`${styles.para} w-100`} placeholder='For INDO INTERNATIONAL TRADING FZCO. (BUYER)'/>
+      <GrowInput type="text" className={`${styles.para} `} placeholder='For INDO INTERNATIONAL TRADING FZCO. (BUYER)'/>
 
       </p>
       
