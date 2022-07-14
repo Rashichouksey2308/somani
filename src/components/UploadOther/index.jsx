e2c93d668ffa758a6ee44d76f0185ef6dbd680d2
Sneha <sneha.yadav@techchefz.com>
2022-07-14 12:02:53 +0530

    feat(IGM,  InspectionDocument, UploadDocument): page created , bugs fixed

--- src/components/UploadOther/index.jsx
+++ src/components/UploadOther/index.jsx
@@ -82,10 +82,10 @@ const index = () => {
           >
             <thead>
               <tr>
-                <th>DOCUMENT NAME <img className={`${styles.sort_image} mb-1`} src="./static/icons8-sort-24.png " alt="Sort icon"/></th>
-                <th>FORMAT <img className={`${styles.sort_image} mb-1`} src="./static/icons8-sort-24.png " alt="Sort icon"/></th>
-                <th>DOCUMENT DATE <img className={`${styles.sort_image} mb-1`} src="./static/icons8-sort-24.png " alt="Sort icon"/></th>
-                <th>UPLOADED BY <img className={`${styles.sort_image} mb-1`} src="./static/icons8-sort-24.png " alt="Sort icon"/></th>
+                <th>DOCUMENT NAME <img className={`${styles.sort_image} mb-1`} src="/static/icons8-sort-24.png " alt="Sort icon"/></th>
+                <th>FORMAT <img className={`${styles.sort_image} mb-1`} src="/static/icons8-sort-24.png " alt="Sort icon"/></th>
+                <th>DOCUMENT DATE <img className={`${styles.sort_image} mb-1`} src="/static/icons8-sort-24.png " alt="Sort icon"/></th>
+                <th>UPLOADED BY <img className={`${styles.sort_image} mb-1`} src="/static/icons8-sort-24.png " alt="Sort icon"/></th>
                 <th>STATUS </th>
                 <th>ACTION</th>
                 <th>
