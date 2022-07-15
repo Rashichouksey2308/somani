b5401a41b6b2d7e1060f6e7dcd53fcbab2b03e67
Sneha <sneha.yadav@techchefz.com>
2022-07-15 16:10:37 +0530

    feat(Warehouse, DischargeCargo, CIMS, BillLading,Document): 2 page created, bugs fixed in other component BREAKING CHANGE:

--- src/components/BillOfEntry/Warehouse/index.jsx
+++ src/components/BillOfEntry/Warehouse/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './index.module.scss'
 import { Form, Row, Col } from 'react-bootstrap'
-import SaveBar from '../SaveBar'
-
+import SaveBar from '../../SaveBar'
+import InspectionDocument from '../../InspectionDocument'
 
 export default function Index() {
   return (
@@ -11,102 +11,77 @@ export default function Index() {
         <div className={`${styles.backgroundMain} container-fluid`}>
         <div className={`${styles.vessel_card} mt-3 border_color`}>
 
-        <div className={`${styles.main} border_color mt-4 card `}>
-        <div
-              className={`${styles.head_container} card-header border_color head_container justify-content-between d-flex bg-transparent`}
+          <div className={`${styles.main} mt-4 card border_color`}>
+            <div
+              className={`${styles.head_container} card-header border_color head_container align-items-center justify-content-between d-flex bg-transparent`}
             >
-              <h3 className={`${styles.heading}`}>CIMS Details</h3>
-              <button className={styles.add_btn}><span className={styles.add_sign}>+</span>Add</button>
-
+              <h3 className={`${styles.heading}`}>Warehouse Details</h3>
+              <span className='ml-3' >+</span>
+          
             </div>
-                <div className={`${styles.dashboard_form} mt-2 card-body`}>
-                  <div className='row'>
-                  <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6`}>
-                  <input
-                    className={`${styles.input_field} input form-control`}
-                    required
-                    type="text"
-                  />
-                  <label className={`${styles.label_heading} label_heading`}>
-                    Vessel Name<strong className="text-danger">*</strong>
-                  </label>
+            <div className={`${styles.dashboard_form} mt-3 card-body`}>
+            <div className='row'>
+               <div className='col-lg-4 col-md-6 col-sm-6'>
+                  <div className={`${styles.label} text`}
+                        >
+                    Commodity
+                  </div>
+                  <span className={styles.value}>Iron</span>
                 </div>
-                <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6`} >
+                    <div className='col-lg-4 col-md-6 col-sm-6'>
+                  <div className={`${styles.label} text`}
+                        >
+                    CMA Name
+                  </div>
+                  <span className={styles.value}>Abcz</span>
+                </div>
+                 <div className='col-lg-4 col-md-6 col-sm-6'>
+                  <div className={`${styles.label} text`}
+                        >
+                    Storage Address 
+                  </div>
+                  <span className={styles.value}>New Dolphin Yard, Visakhapatnam Port</span>
+                </div>
+                <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6 mt-5`} >
                     <input 
                     className={`${styles.input_field} input form-control`} type="number"/> 
                      <label className={`${styles.label_heading} label_heading`}>
                       Quantity<strong className="text-danger">*</strong></label>                   
                 </div> 
-                <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6`} >
+                <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6 mt-5`} >
                     <input 
-                    className={`${styles.input_field} input form-control`}  required type="text"/> 
+                    className={`${styles.input_field} input form-control`} type="date"/> 
                      <label className={`${styles.label_heading} label_heading`}>
-                      CIRC Number<strong className="text-danger">*</strong></label>                   
+                      Date of Storage<strong className="text-danger">*</strong></label>                   
                 </div> 
-                <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6`}>
-                  <input
-                    className={`${styles.input_field} input form-control`}
-                    
-                    type="date"
+                
+                 <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6 mt-5`} >
+                  <div className='d-flex justify-content-start mt-2'>
+                  <div className={styles.uploadBtnWrapper}>
+                  <input type="file" name="myfile" />
+                  <button className={`${styles.upload_btn} btn`}>
+                    Upload
+                  </button>
+                </div>
+                   <img
+                    src="/static/delete 2.svg"
+                    className={`${styles.delete_image} img-fluid ml-3 mr-3`}
+                    alt="Bin"
                   />
-                  <label className={`${styles.label_heading} label_heading`}>
-                    CIRC Date<strong className="text-danger">*</strong>
-                  </label>
+                   <img
+                      src="/static/mode_edit.svg"
+                      className={`${styles.edit_image} img-fluid`}
+                      alt="edit"
+                     
+                    />
+                </div> 
                 </div>
-                <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6`} >
-                    <input 
-                    className={`${styles.input_field} input form-control`} type="number"/> 
-                     <label className={`${styles.label_heading} label_heading`}>
-                      CIMS Charges<strong className="text-danger">*</strong></label>                   
-                </div> 
-                <div className={`${styles.form_group} col-lg-4 col-md-6 col-sm-6`} >
-                <select
-                    className={`${styles.input_field} input form-control`}>
-                    <option>Indo German</option>
-                    <option>N/A</option>
-                  </select>
-                     <label className={`${styles.label_heading} label_heading`}>
-                      Payment by<strong className="text-danger">*</strong></label>                   
-                </div> 
-               
                   </div>
-
-                </div>
-                <div className={styles.table_scroll_outer}>
-               <div className={styles.table_scroll_inner}>
-                <table className={`${styles.table} table mt-5`} cellPadding="0" cellSpacing="0" border="0">
-                    <thead>
-                        <tr>
-                            <th>DOCUMENT NAME <img className={`${styles.sort_image} mb-1`} src = "/static/icons8-sort-24.png" alt="Sort icon"/></th>
-                            <th>FORMAT <img className={`${styles.sort_image} mb-1`} src ="/static/icons8-sort-24.png" alt="Sort icon"/></th>
-                            <th>DOCUMENT DATE <img className={`${styles.sort_image} mb-1`} src ="/static/icons8-sort-24.png" alt="Sort icon"/></th>
-                            <th>ACTION</th>
-                        </tr>
-                        </thead>                        
-                        <tbody>
-                      
-                        <tr className='table_row'>
-                            <td className={styles.doc_name}>BL Acknowledgement Copy<strong className="text-danger ml-0">*</strong>
-                           </td>
-                            <td><img src="/static/pdf.svg" className="img-fluid" alt="Pdf" /></td>
-                            <td className={styles.doc_row}>28-02-2022,5:30 PM</td>
-                            <td> 
-                            <div className={styles.uploadBtnWrapper}>
-                              <input type="file" name="myfile" />
-                              <button className={`${styles.upload_btn} btn`}>
-                                Upload
-                              </button>
-                            </div>
-                        </td>
-                        </tr>
-                              
-                    </tbody>
-                </table>
-                </div>
-                </div>
-            </div>
            
-              <div className={`${styles.upload_main} mt-4 mb-5 upload_main`}>
+            </div>
+        
+          </div>
+        <div className={`${styles.upload_main} mt-4 mb-5 upload_main`}>
           <div
             className={`${styles.head_container} border_color d-flex justify-content-between`}
             data-toggle="collapse"
@@ -165,7 +140,7 @@ export default function Index() {
                     </Form.Group>
                     <div className={styles.uploadBtnWrapper}>
                       <input type="file" name="myfile" />
-                      <button className={`${styles.upload_button} btn`}>
+                      <button className={`${styles.upload_button} mt-4  btn`}>
                         Upload
                       </button>
                     </div>
@@ -173,9 +148,9 @@ export default function Index() {
                 </div>
               </Form>
             </div>
-            <div className={`${styles.search_container} search_container  d-flex justify-content-between pt-3 pl-3 pr-3`}>
+            <div className={`${styles.search_container} search_container d-flex justify-content-between pt-3 pl-3 pr-3`}>
           <div>
-          <select className={`${styles.dropDown} formControl input form-control`} >
+          <select className={`${styles.dropDown} input formControl form-control`} >
                   <option value="volvo">Loading, Transit, Unloading</option>
                   <option value="India">India</option>
                  
@@ -269,9 +244,9 @@ export default function Index() {
             </div>
           </div>
         </div>
+          </div>
+          <SaveBar/>
 
-      </div>
-           <SaveBar/>
               </div>
            </>
 
