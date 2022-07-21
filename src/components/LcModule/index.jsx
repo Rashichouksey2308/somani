8860d44bac567dfcb74c315710c468ea6e212e48
Sneha <sneha.yadav@techchefz.com>
2022-07-21 14:24:41 +0530

    fix(LcApplication, LetterCredit): bugs fixed BREAKING CHANGE:

--- src/components/LcModule/index.jsx
+++ src/components/LcModule/index.jsx
@@ -15,7 +15,7 @@ function Index() {
       
           <div className={styles.head_header}>
               <img
-                className={`${styles.arrow} img-fluid`}
+                className={`${styles.arrow} img-fluid mr-2`}
                 src="/static/keyboard_arrow_right-3.svg"
                 alt="ArrowRight"
               />
