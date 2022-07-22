78cdcf4b79d9c9df47352f55a9e7e4ce6d9d127b
Nitin <nitin.lomod@techchez.com>
2022-07-22 13:02:27 +0530

    fix(Review Queue): Review Queue - Bug Fixes BREAKING CHANGE:

--- src/components/SaveBar/index.jsx
+++ src/components/SaveBar/index.jsx
@@ -5,8 +5,8 @@ import styles from './index.module.scss'
 function index(props) {
   return (
     <div className={`${styles.root} card`}>
-        <div className={styles.reject}><span>Save</span></div>
-        <div className={styles.approve}><span >Submit</span></div>
+        <div className={`${styles.reject} ml-3`}><span>Save</span></div>
+        <div className={`${styles.approve} ml-3`}><span >Submit</span></div>
     </div>
   )
 }
