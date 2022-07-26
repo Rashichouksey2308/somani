1db8df4bf72579eca9d990627e703208ae3ed09d
Nitin <nitin.lomod@techchez.com>
2022-07-27 01:30:56 +0530

    fix(Review - Financials): Review - Financials Bug Fixes BREAKING CHANGE:

--- src/components/ReviewQueueFinancials/Peer/index.jsx
+++ src/components/ReviewQueueFinancials/Peer/index.jsx
@@ -10,13 +10,8 @@ function Index({peerData}) {
 
     <>
       <div className={`${styles.card} card`}>
-        <div
-          className={`${styles.cardHeader} card-header d-flex align-items-center justify-content-between p-3 bg-transparent`}
-          data-toggle="collapse"
-          data-target="#peerComparison"
-          aria-expanded="true"
-          aria-controls="peerComparison"
-        >
+        <div className={`${styles.cardHeader} card-header d-flex align-items-center justify-content-between p-3 bg-transparent`} data-toggle="collapse" data-target="#peerComparison"
+          aria-expanded="true" aria-controls="peerComparison">
           <h2 className="mb-0">Peer Comparison</h2>
           <div className={`${styles.unit_container} d-flex align-items-center`}>
             <h5 className={`${styles.unit_label} accordion_Text`}>Units :</h5>
@@ -26,86 +21,78 @@ function Index({peerData}) {
             <span>+</span>
           </div>
         </div>
-        <div
-          id="peerComparison"
-          className="collapse"
-          aria-labelledby="peerComparison"
-          data-parent="#FinancialsAccordion"
-        >
-          <div
-            className={`${styles.noBorderTable} ${styles.cardBody} card-body border_color`}
-          >
-            <table
-              className={`${styles.table} table`}
-              cellPadding="0"
-              cellSpacing="0"
-              border="0"
-            >
-              <thead>
-                <tr>
-                  <th width="30%">COMPANY</th>
-                  <th className="text-center" width="14%">
-                    FY ENDING
-                  </th>
-                  <th className="text-center" width="14%">
-                    REVENUE
-                  </th>
-                  <th className="text-center" width="14%">
-                    EBDITA MARGIN (%)
-                  </th>
-                  <th className="text-center" width="14%">
-                    PAT MARGIN (%)
-                  </th>
-                  <th className="text-center" width="14%">
-                    BORROWINGS
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-               {peerData && peerData?.financial?.peerComparison?.map((peers, index) => ( <tr key={index}>
-                  <td>{peers.name}</td>
-                  <td className="text-center">{moment(peers?.finyrEnddate)
-                      .format('MMM-YY')
-                      .toUpperCase()}</td>
-                  <td className="text-center">{peers.revenue}</td>
-                  <td className="text-center">{peers.ebidtaMargin}</td>
-                  <td className="text-center">{peers.patMargin}</td>
-                  <td className="text-center">{peers.borrowings}</td>
-                </tr>))}
-                {/* <tr>
-                  <td>Ascent Hotels Private Limited</td>
-                  <td className="text-center">Mar-2018</td>
-                  <td className="text-center">96.17</td>
-                  <td className="text-center">45.26%</td>
-                  <td className="text-center">45.26%</td>
-                  <td className="text-center">285.01</td>
-                </tr>
-                <tr>
-                  <td>Orange Country Resorts & Hotels Limited</td>
-                  <td className="text-center">Mar-2018</td>
-                  <td className="text-center">96.17</td>
-                  <td className="text-center">45.26%</td>
-                  <td className="text-center">45.26%</td>
-                  <td className="text-center">285.01</td>
-                </tr>
-                <tr>
-                  <td>Divine Infracon Private Limited</td>
-                  <td className="text-center">Mar-2018</td>
-                  <td className="text-center">96.17</td>
-                  <td className="text-center">45.26%</td>
-                  <td className="text-center">45.26%</td>
-                  <td className="text-center">285.01</td>
-                </tr>
-                <tr>
-                  <td>Gujarat Jhm Hotels Limited</td>
-                  <td className="text-center">Mar-2018</td>
-                  <td className="text-center">96.17</td>
-                  <td className="text-center">45.26%</td>
-                  <td className="text-center">45.26%</td>
-                  <td className="text-center">285.01</td>
-                </tr> */}
-              </tbody>
-            </table>
+        <div id="peerComparison" className="collapse" aria-labelledby="peerComparison" data-parent="#FinancialsAccordion">
+          <div className={`${styles.noBorderTable} ${styles.cardBody} p-0 card-body border_color`}>
+            <div className={styles.table_scroll_outer}>
+              <div className={styles.table_scroll_inner}>
+                <table className={`${styles.table} table`} cellPadding="0" cellSpacing="0" border="0">
+                  <thead>
+                    <tr>
+                      <th width="30%">COMPANY</th>
+                      <th className="text-center" width="14%">
+                        FY ENDING
+                      </th>
+                      <th className="text-center" width="14%">
+                        REVENUE
+                      </th>
+                      <th className="text-center" width="14%">
+                        EBDITA MARGIN (%)
+                      </th>
+                      <th className="text-center" width="14%">
+                        PAT MARGIN (%)
+                      </th>
+                      <th className="text-center" width="14%">
+                        BORROWINGS
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {peerData && peerData?.financial?.peerComparison?.map((peers, index) => ( <tr key={index}>
+                      <td>{peers.name}</td>
+                      <td className="text-center">{moment(peers?.finyrEnddate)
+                          .format('MMM-YY')
+                          .toUpperCase()}</td>
+                      <td className="text-center">{peers.revenue}</td>
+                      <td className="text-center">{peers.ebidtaMargin}</td>
+                      <td className="text-center">{peers.patMargin}</td>
+                      <td className="text-center">{peers.borrowings}</td>
+                    </tr>))}
+                    {/* <tr>
+                      <td>Ascent Hotels Private Limited</td>
+                      <td className="text-center">Mar-2018</td>
+                      <td className="text-center">96.17</td>
+                      <td className="text-center">45.26%</td>
+                      <td className="text-center">45.26%</td>
+                      <td className="text-center">285.01</td>
+                    </tr>
+                    <tr>
+                      <td>Orange Country Resorts & Hotels Limited</td>
+                      <td className="text-center">Mar-2018</td>
+                      <td className="text-center">96.17</td>
+                      <td className="text-center">45.26%</td>
+                      <td className="text-center">45.26%</td>
+                      <td className="text-center">285.01</td>
+                    </tr>
+                    <tr>
+                      <td>Divine Infracon Private Limited</td>
+                      <td className="text-center">Mar-2018</td>
+                      <td className="text-center">96.17</td>
+                      <td className="text-center">45.26%</td>
+                      <td className="text-center">45.26%</td>
+                      <td className="text-center">285.01</td>
+                    </tr>
+                    <tr>
+                      <td>Gujarat Jhm Hotels Limited</td>
+                      <td className="text-center">Mar-2018</td>
+                      <td className="text-center">96.17</td>
+                      <td className="text-center">45.26%</td>
+                      <td className="text-center">45.26%</td>
+                      <td className="text-center">285.01</td>
+                    </tr> */}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
