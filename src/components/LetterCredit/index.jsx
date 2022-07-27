198f3a305d84bf2e6b5d92425c14bba829e6e525
Nitin <nitin.lomod@techchez.com>
2022-07-27 11:55:28 +0530

    feat(Review, Filter): Review - primary accound field added , Filter - LC module BREAKING CHANGE:

--- src/components/LetterCredit/index.jsx
+++ src/components/LetterCredit/index.jsx
@@ -53,9 +53,24 @@ function Index() {
             />
           </div>
         </div>
-        <a className={styles.filterIcon}>
-          <img src="/static/filter.svg" className="img-fluid" alt="Filter" />
-        </a>
+        <div className={`${styles.dropdown} dropdown`}>
+          <a className={`${styles.filterIcon}`} id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="/static/filter.svg" className="img-fluid" alt="Filter" />
+          </a>
+          <div className={`${styles.dropdown_menu} dropdown-menu`} aria-labelledby="dropdownMenu2">
+            <div className={`${styles.dropdown_header} dropdown-header d-flex align-items-center`}>
+              <img src="/static/filter_alt.svg" className={`${styles.filter_icon} img-fluid`} alt="Filter Small" />
+              <h3>Filter</h3>
+              <img src="/static/close-2.svg" className={`${styles.close} img-fluid ml-auto`} alt="Close" />
+            </div>
+            <div className={`${styles.filter_list} dropdown-item`} href="#"><input type="checkbox" id="orderId" name="orderId" value="Order ID"/> <label for="orderId">Order ID</label></div>
+            <div className={`${styles.filter_list} dropdown-item`} href="#"><input type="checkbox" id="commodity" name="commodity" value="Commodity"/> <label for="commodity">Commodity</label></div>
+            <div className={`${styles.filter_list} dropdown-item`} href="#"><input type="checkbox" id="status" name="status" value="Status"/> <label for="status">Status</label></div>
+            <div className={`${styles.filter_list} dropdown-item`} href="#"><input type="checkbox" id="createdBy" name="createdBy" value="Created by"/> <label for="createdBy">Created by</label></div>
+            <button className={`${styles.btn} btn`} type='button'>Apply Filter</button>
+          </div>
+        </div>
+        
         {/* <a href="#" className={`${styles.filterList} filterList `}>
         Bhutani Traders
        <img src="/static/close-b.svg" className="img-fluid" alt="Close" />
@@ -68,10 +83,8 @@ function Index() {
        Create</button> */}
       </div>
 
-      <div className={`${styles.datatable} datatable border-color`}>
-        <div
-          className={`${styles.tableFilter} card d-flex justify-content-between`}
-        >
+      <div className={`${styles.datatable} card datatable border-color`}>
+        <div className={`${styles.tableFilter} d-flex justify-content-between`}>
           <h3 className="heading_card">Letter of Credit</h3>
         </div>
         <div className={styles.table_scroll_outer}>
