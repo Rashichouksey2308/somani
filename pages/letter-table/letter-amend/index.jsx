d343253a4996ba48871d8803dd92a17e1c274bb9
Sneha <sneha.yadav@techchefz.com>
2022-07-28 14:44:51 +0530

    fix(AmmendLetter): bugs fix BREAKING CHANGE:

--- pages/letter-table/letter-amend/index.jsx
+++ pages/letter-table/letter-amend/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import AmendLetter from '../../src/components/AmendLetter'
+import AmendLetter from '../../../src/components/AmendLetter'
 
 const Index = () => {
-  return (
-   <AmendLetter/>
-  )
+  return <AmendLetter />
 }
 
 export default Index
