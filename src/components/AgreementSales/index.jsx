cebd9561b3ef8f0fd9a9d218596fb05d850ce9e6
VishwaVijay <vishwavijay.bisht@techchefz.com>
2022-08-01 14:14:13 +0530

    build fiox

--- src/components/AgreementSales/index.jsx
+++ src/components/AgreementSales/index.jsx
@@ -674,7 +674,7 @@ const payment=()=>{
      <div className={`${styles.paymet} card-body`}>
      <div className={`d-flex justify-content-between align-items-between`}>
        <input placeholder={`5000 Wet Metric Tons (Wmt) +/- 10Pct Of Mmd: Manganeseore Of Gabon Origin (44,50Pct Mn Typical - 5Pct Moisture), Ciffo Visakhapatnam Port Packing In Bulk.`}></input>
-       <img class="img-fluid ml-4" src="/static/add-btn.svg" alt="add button"></img>
+       <img className="img-fluid ml-4" src="/static/add-btn.svg" alt="add button"></img>
      </div>
      <div className={`${styles.button_container} d-flex justify-content-start  align-items-center `}>
         <div className={`${styles.button} d-flex justify-content-center align-items-center`}>
@@ -688,8 +688,8 @@ const payment=()=>{
      <div className={`d-flex justify-content-between align-items-center ${styles.comment}`}>
        <input placeholder={`5000 Wet Metric Tons (Wmt) +/- 10Pct Of Mmd: Manganeseore Of Gabon Origin (44,50Pct Mn Typical - 5Pct Moisture), Ciffo Visakhapatnam Port Packing In Bulk.`}></input>
        <div className={`d-flex justify-content-evenly align-items-center`}>
-         <img class="img-fluid ml-4" src="/static/add-btn.svg" alt="add button"></img>
-         <img src="/static/delete 2.svg" class="img-fluid" alt="delete"></img>
+         <img className="img-fluid ml-4" src="/static/add-btn.svg" alt="add button"></img>
+         <img src="/static/delete 2.svg" className="img-fluid" alt="delete"></img>
        </div>
      </div>
      </div>
