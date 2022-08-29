c84c7436623900bf2c1e775ae38b328e34635f8b
prashant <prashant008.pr@gmail.com>
2022-08-29 15:58:17 +0530

    fix(Termsheet): search functionality BREAKING CHANGE:

--- pages/termsheet/index.jsx
+++ pages/termsheet/index.jsx
@@ -1,14 +1,4 @@
 
-// import TermSheet from '../../src/components/TermSheet'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-// export default function Home() {
-//   return (
-//     <TermSheet />
-//   )
-// }
-
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import TermSheetMain from '../../src/components/TermSheetMain'
