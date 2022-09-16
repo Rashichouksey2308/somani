64a5b8f9e183508c03689f4d7c4ea25da53d3bdb
Harsh bhadana <harsh.bhadana@techchefz.com>
2022-09-16 18:03:27 +0530

    fix(utils, register, ): bug fixes

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -33,7 +33,18 @@ export const CovertvaluefromtoCR = (value, conversionValue = 10000000) => {
   return newValue
 }
 
-export const convertValue = (value, coversionRate = 10000000) => {
+Number.prototype.toFixedNoRounding = function (n) {
+  const reg = new RegExp("^-?\\d+(?:\\.\\d{0," + n + "})?", "g")
+  const a = this.toString().match(reg)[0];
+  const dot = a.indexOf(".");
+  if (dot === -1) { // integer, insert decimal dot and pad up zeros
+    return a + "." + "0".repeat(n);
+  }
+  const b = n - (a.length - dot) + 1;
+  return b > 0 ? (a + "0".repeat(b)) : a;
+}
+
+export const convertValue = (value, coversionRate = 10000000, toFixed = 2) => {
   let newValue = Number(value / coversionRate)
   if (value === 0) {
     // console.log(, 'invalid value')
@@ -43,7 +54,7 @@ export const convertValue = (value, coversionRate = 10000000) => {
     console.log(newValue, value, 'inspectionData231')
     return ''
   } else {
-    return newValue
+    return newValue.toFixedNoRounding(2)
   }
 }
 export const predictiveSearch = (text) => {
