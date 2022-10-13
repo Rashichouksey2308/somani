8955d19a4d10224f39887683f5af265677abb638
prashant <prashant008.pr@gmail.com>
2022-10-13 16:47:47 +0530

    fix

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -30,7 +30,7 @@ export const phoneValidation = (phone) => {
 
 export const gSTINValidation = (gstin) => {
   var regex = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/
-  if (gstin.match(regex)) {
+  if (gstin.toUpperCase().match(regex)) {
     console.log('trueGstinValidation',)
     return true
   } else {
