5326c5b0a9781eef33f263f260db988121ac8805
VishwaVijay <vishwavijay.bisht@techchefz.com>
2022-10-21 16:30:55 +0530

    something went wrong fix

--- src/components/BillOfEntry/index.jsx
+++ src/components/BillOfEntry/index.jsx
@@ -556,7 +556,7 @@ export default function Index({
       setDutyData([...duty11])
       setBillOfEntryData([...tempArray])
     }
-  }, [customData])
+  }, [])
 
   console.log(dutyData,"setDutydata")
   // console.log(
