b11e425e1ada5e63f00488be34ae3620a1a6d21b
Niteesh Singh <niteeshsingh5@gmail.com>
2022-11-08 19:24:04 +0530

    ui fixes and functionality refactoring

--- src/components/Commodities/index.jsx
+++ src/components/Commodities/index.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import styles from './index.module.scss';
 import { Card } from 'react-bootstrap';
 
+const tempArr = [
+  { percentage: `100%` },
+  { percentage: `80%` },
+  { percentage: `60%` },
+  { percentage: `50%` },
+  { percentage: `40%` },
+];
+
 function Index({ data }) {
-  let tempArr = [
-    { name: 'Iron', val: '1,837', val2: 1837, percentage: `100%` },
-    { name: 'Steel', val: '1,341', val2: 1837, percentage: `80%` },
-    { name: 'Aluminium', val: '1,000', val2: 1837, percentage: `60%` },
-    { name: 'Copper', val: '600', val2: 1837, percentage: `50%` },
-    { name: 'Brass', val: '400', val2: 1837, percentage: `40%` },
-  ];
   return (
     <Card className={`${styles.card} border`}>
       <Card.Header className={`${styles.header} border_color heading_card`}>Top 5 Commodities </Card.Header>
-      {/* <hr className={styles.hr}/> */}
       <Card.Body className={styles.body}>
         {data
           .sort((a, b) => parseFloat(b.total) - parseFloat(a.total))
