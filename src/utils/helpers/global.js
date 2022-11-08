f9aaa487f38b4a91118c28bc5a86244e9dd6e5a0
Nishant Bhadana <35363299+nishantbhadana@users.noreply.github.com>
2022-11-08 17:37:40 +0530

    Object Validation Helper Added

--- src/utils/helpers/global.js
+++ src/utils/helpers/global.js
@@ -1,13 +1,24 @@
-import React from 'react'
 import Cookies from 'js-cookie'
+import { toast } from 'react-toastify'
+import has from 'lodash/has'
+const _ = { has }
 
 export const returnAuthToken = () => {
   const cookie = Cookies.get('SOMANI')
   const decodedString = Buffer.from(cookie, 'base64').toString('ascii')
-  const [userId, refreshToken, jwtAccessToken] = decodedString.split('#')
+  const [jwtAccessToken] = decodedString.split('#')
   return jwtAccessToken
 }
 
+/**
+ * It takes a number, and returns a string with the number formatted with commas
+ * @param number - The number you want to format.
+ * @param [locales] - The locale to use.
+ * @param [maximum=0] - The maximum number of digits after the decimal point.
+ * @param [minimum=0] - The minimum number of digits to be used. If the minimum number of digits is not specified, it is
+ * assumed to be 0.
+ * @returns A function that takes in a number, locales, maximum, and minimum.
+ */
 export const returnReadableNumber = (number, locales = undefined, maximum = 0, minimum = 0) => {
   if (number) {
     return number.toLocaleString(locales, {
@@ -15,4 +26,42 @@ export const returnReadableNumber = (number, locales = undefined, maximum = 0, m
       minimumFractionDigits: minimum
     })
   }
+}
+
+/**
+ * If the toast message is not active, then display the toast message
+ * @param toastMessage - The message you want to display in the toast.
+ * @returns A function that takes in a string and returns a toast.
+ */
+export const handleErrorToast = (toastMessage) => {
+  if (!toast.isActive(toastMessage.toUpperCase())) {
+    return toast.error(toastMessage.toUpperCase(), { toastId: toastMessage })
+  }
+}
+
+/**
+ * If the toast message is not active, then return a success toast with the message and a toastId
+ * @param toastMessage - The message you want to display in the toast
+ * @returns A toast message
+ */
+export const handleSuccessToast = (toastMessage) => {
+  if (!toast.isActive(toastMessage.toUpperCase())) {
+    return toast.success(toastMessage.toUpperCase(), { toastId: toastMessage })
+  }
+}
+
+/**
+ * It takes an object and an array of objects with keys and error messages. It then checks if the object has the keys in
+ * the array and if not, it displays the error message
+ * @param [obj] - The object to be validated
+ * @param [validation] - [{key: 'name', error: 'Name is required'}, {key: 'email', error: 'Email is required'}]
+ */
+export const objectValidator = (obj = {}, validation = []) => {
+  validation.every(element => {
+    if (!_.has(obj, element.key)) {
+      handleErrorToast(element.error)
+      return false
+    }
+    return true
+  })
 }
