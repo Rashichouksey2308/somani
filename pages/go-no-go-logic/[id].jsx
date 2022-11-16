83acbd3e8ec4386e132086832d802ca8750ed4bf
sneha3391 <sneha.yadav@techchefz.com>
2022-11-16 11:55:58 +0530

    feat(Document master Rounting done): BREAKING CHANGE:

--- pages/go-no-go-logic/[id].jsx
+++ pages/go-no-go-logic/[id].jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './index.module.scss'
 import { Card } from 'react-bootstrap';
 import Router from 'next/router';
-import AddCommodity from '../../src/components/AddCommodity';
+import Image from 'next/image'
 
 function Index() {
   return (
@@ -12,7 +12,7 @@ function Index() {
           <div className={`${styles.head_header} align-items-center`}>
             <div onClick={() => Router.push('/commodity')} style={{ cursor: 'pointer' }}>
               <img
-                className={`${styles.arrow} img-fluid image_arrow mr-2`}
+                className={`${styles.back_arrow} img-fluid image_arrow mr-2`}
                 src="/static/keyboard_arrow_right-3.svg"
                 alt="ArrowRight"
               />
@@ -20,7 +20,89 @@ function Index() {
             <h1 className={styles.heading}>Go No Go Logic</h1>
           </div>
         </Card.Header>
-        {/* <AddCommodity /> */}
+        <div className={`${styles.backgroundMain}`}>
+      <div className={`${styles.vessel_card} border_color`}>
+        <div className={`${styles.main} vessel_card card border_color`}>
+          <div
+            className={`${styles.down_container} card-header border_color d-flex justify-content-between bg-transparent`}
+            data-toggle="collapse"
+            data-target="#authorisedDetails"
+            aria-expanded="true"
+            aria-controls="authorisedDetails"
+          >
+            <h3 className={`${styles.heading} mb-0`}>Go No Go Details</h3>
+            <span>+</span>
+          </div>
+          <div id="authorisedDetails" aria-labelledby="authorisedDetails">
+            <div className={`${styles.dashboard_form} card-body`}>
+             
+                  <div  className="row">
+                    <div className={`${styles.form_group} col-lg-3 col-md-6 col-sm-6 `}>
+                     <div>Transaction Type</div>
+                    </div>
+                    <div className={`${styles.form_group} col-lg-9 col-md-12 `}>
+                      <div className="d-flex">
+                        <select
+                          className={`${styles.input_field} ${styles.customSelect} border_color input form-control`}
+                        >
+                          <option value="">Leads</option>
+                          <option value="">Agreement</option>
+                        </select>
+                        <label className={`${styles.label_heading} label_heading`}>
+                          Module <strong className="text-danger">*</strong>
+                        </label>
+                        <div className={`${styles.img_arrow} image_arrow`}>
+                          <Image width="13px" height="8px" src="/static/inputDropDown.svg" alt="Search" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className={`${styles.form_group} col-lg-3 col-md-6 col-sm-6 `}>
+                      <div className="d-flex">
+                        <select
+                          className={`${styles.input_field} ${styles.customSelect} border_color input form-control`}
+                        >
+                          <option value="">Credit Queue</option>
+                          <option value="">Profile</option>
+                        </select>
+                        <label className={`${styles.label_heading} label_heading`}>
+                          Sub-Module <strong className="text-danger">*</strong>
+                        </label>
+                        <div className={`${styles.img_arrow} image_arrow`}>
+                          <Image width="13px" height="8px" src="/static/inputDropDown.svg" alt="Search" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className={`${styles.form_group} col-lg-3 col-sm-6`}>
+                      <div className="d-flex justify-content-start align-items-center mt-2">
+                      
+                          <img
+                            src="/static/delete 2.svg"
+                            className={`${styles.delete_image} p-0 mr-4 border-0`}
+                            alt="delete"
+                            onClick={() => {
+                              onDeleteRow(index);
+                            }}
+                          />
+                       
+                        {/* {index === 0 && ( */}
+                        <img
+                          className={`${styles.plus_add}`}
+                          src="/static/add-btn.svg"
+                          alt="Plus"
+                          onClick={(e) => {
+                            onAddRow();
+                          }}
+                        />
+                        {/* )} */}
+                      </div>
+                    </div>
+                  </div>
+               
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
       </Card>
     </div>
   );
