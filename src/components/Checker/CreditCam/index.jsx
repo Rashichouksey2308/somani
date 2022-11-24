ce2152f5765a1db88ce50a794e5999eeaa88d285
Rashi Chouksey <rashi.chouksey@rashiss544.successivetech.com>
2022-11-24 11:00:40 +0530

    Work In Progress

---
+++ src/components/Checker/CreditCam/index.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './index.module.scss';
+import {basicInfo} from '../../../components/'
+
+
+function Index(){  
+  return (
+  <div>index
+      <basicInfo />
+  </div>
+  )
+}
+
+export default Index;
