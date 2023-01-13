1bf22367826f3d093d2fe82ce2266098038aeccd
Kapil Gurnani <kapil.gurnani@kapilss453.successivetech.com>
2023-01-13 20:06:22 +0530

    merge mvp2 code to development

--- pages/leads/index.jsx
+++ pages/leads/index.jsx
@@ -5,7 +5,7 @@ import _ from 'lodash';
 import 'bootstrap/dist/css/bootstrap.css';
 import Router from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
-import { GetAllUpdatedBuyer, GetOrderLeads, GetOrders,GetAllBuyer } from '../../src/redux/registerBuyer/action';
+import { GetAllUpdatedBuyer, GetOrderLeads, GetOrders, GetAllBuyer } from '../../src/redux/registerBuyer/action';
 import { FilterLeads } from '../../src/redux/buyerProfile/action.js';
 import { setDynamicName, setPageName } from '../../src/redux/userData/action';
 import SearchAndFilter from '../../src/components/SearchAndFilter';
@@ -18,9 +18,9 @@ import Filter from '../../src/components/Filter';
 import Pagination from '../../src/components/Pagination';
 import { SearchLeads } from '../../src/redux/buyerProfile/action.js';
 import styles from './index.module.scss';
-import constants from '@/utils/constants'
+import constants from '@/utils/constants';
 
-export default function Index() {
+function Index() {
   const dispatch = useDispatch();
 
   const { updatedBuyerList, getOrderLeads } = useSelector((state) => state.buyer);
@@ -79,7 +79,7 @@ export default function Index() {
 
     showBadges.map((item) => {
       query = query + `&${item?.key}=${slugify(item?.displayVal, { lower: false })}`;
-    })
+    });
 
     setFilterQuery(query);
 
@@ -99,12 +99,10 @@ export default function Index() {
         if (val === 'status') {
           result?.status && badgesItems.push({ key: val, displayVal: result?.status });
           query = query + `&${val}=${result?.status}`;
-        }
-        else if (val === 'company_name') {
+        } else if (val === 'company_name') {
           result?.buyerName && badgesItems.push({ key: val, displayVal: result?.buyerName });
           query = query + `&${val}=${slugify(result?.buyerName, { lower: false })}`;
-        }
-        else if (val === 'commodity') {
+        } else if (val === 'commodity') {
           result?.commodity && badgesItems.push({ key: val, displayVal: result?.commodity });
           query = query + `&${val}=${slugify(result?.commodity, { lower: false })}`;
         }
@@ -147,11 +145,11 @@ export default function Index() {
         }
       });
       delayedQuery(queryParams);
-    if (query.length >= constants.numberThree) {
-      dispatch(SearchLeads(query));
+      if (query.length >= constants.numberThree) {
+        dispatch(SearchLeads(query));
+      }
     }
   };
-
   const handleApplyFilter = () => {
     setAppliedFilters(filterItem);
   };
@@ -227,12 +225,12 @@ export default function Index() {
     {
       Header: 'Order Value',
       accessor: 'orderValue',
-      Cell: ({ value }) => `${(value.toLocaleString('en-US'))} USD`
+      Cell: ({ value }) => `${value.toLocaleString('en-US')} USD`,
     },
     {
       Header: 'Creation Date',
       accessor: 'createdAt',
-      Cell: ({ value }) => value.slice(0, 10)
+      Cell: ({ value }) => value.slice(0, 10),
     },
     {
       Header: 'Existing Customer',
@@ -251,22 +249,32 @@ export default function Index() {
 
   const searchView = () => {
     return (
-      filter && openList && searchterm.length > 3 &&
-      <div className={styles.searchResults}>
-        <ul>
-          {filteredLeads?.data?.data?.length > 0 ? filteredLeads?.data?.data?.map((results, index) => (
-            <li onClick={() => handleListClose(results)} id={results._id} key={index} className="cursor-pointer">
-              {appliedFilters?.company_name === true && results?.buyerName}
-              <span>
-                &nbsp; {appliedFilters?.commodity === true && <span className='text-right'>{results?.commodity}</span>}
-                &nbsp; {appliedFilters?.status === true && <span className='text-right'>{results?.status}</span>}
-              </span>
-            </li>
-          )) : <li><span>No result found</span></li>}
-        </ul>
-      </div>
-    )
-  }
+      filter &&
+      openList &&
+      searchterm.length > 3 && (
+        <div className={styles.searchResults}>
+          <ul>
+            {filteredLeads?.data?.data?.length > 0 ? (
+              filteredLeads?.data?.data?.map((results, index) => (
+                <li onClick={() => handleListClose(results)} id={results._id} key={index} className="cursor-pointer">
+                  {appliedFilters?.company_name === true && results?.buyerName}
+                  <span>
+                    &nbsp;{' '}
+                    {appliedFilters?.commodity === true && <span className="text-right">{results?.commodity}</span>}
+                    &nbsp; {appliedFilters?.status === true && <span className="text-right">{results?.status}</span>}
+                  </span>
+                </li>
+              ))
+            ) : (
+              <li>
+                <span>No result found</span>
+              </li>
+            )}
+          </ul>
+        </div>
+      )
+    );
+  };
 
   return (
     <>
